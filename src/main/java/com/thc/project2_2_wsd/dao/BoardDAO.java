1aa6525afe02da4bba0f9954a90473908b38aac1
Gooddandelion <qkrgjsdlf0324@gmail.com>
2025-11-21 17:08:38 +0900

    풀 리퀘스트 병합 #2
    * fix: add DB column (password, category)

--- src/main/java/com/thc/project2_2_wsd/dao/BoardDAO.java
+++ src/main/java/com/thc/project2_2_wsd/dao/BoardDAO.java
@@ -14,7 +14,7 @@ public class BoardDAO {
     PreparedStatement pstmt = null;
     ResultSet rs = null;
 
-    private final String BOARD_INSERT = "insert into BOARD(title, writer, content) values(?,?,?)";
+    private final String BOARD_INSERT = "insert into BOARD(title, writer, password, category, content) values(?,?,?,?,?)";
 
     public int insertBoard(BoardVO vo) {
         try {
@@ -22,11 +22,14 @@ public class BoardDAO {
             pstmt = con.prepareStatement(BOARD_INSERT);
             pstmt.setString(1, vo.getTitle());
             pstmt.setString(2, vo.getWriter());
-            pstmt.setString(3, vo.getContent());
+            pstmt.setString(3, vo.getPassword());
+            pstmt.setString(4, vo.getCategory());
+            pstmt.setString(5, vo.getContent());
             pstmt.executeUpdate();
             return 1;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
 }
